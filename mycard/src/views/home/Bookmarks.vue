ad0475046214fa4ef4de61c406ec65a6b3f2c2b2
chenyicai0819 <2495842975@qq.com>
2021-12-26 13:57:30 +0800

    修复移动端显示bug

--- mycard/src/views/home/Bookmarks.vue
+++ mycard/src/views/home/Bookmarks.vue
@@ -1,14 +1,33 @@
 <template>
-  <div id="bookmarks">
+  <div :id="ismoblie==true?'bookmarks-moblic':'bookmarks-PC'">
     <Marks/>
   </div>
 </template>
 
 <script>
 import Marks from "./Marks";
+import {reactive, toRefs} from "vue";
 export default {
   name: "Bookmarks",
-  components: {Marks}
+  components: {Marks},
+  setup(){
+    const data=reactive({
+      ismoblie:false,
+    })
+    const isMobile = () => {
+      const flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+      if (flag) {
+        data.ismoblie=true
+      } else {
+        data.ismoblie=false
+      }
+    }
+
+    isMobile();
+    return{
+      ...toRefs(data),isMobile,
+    }
+  }
 }
 </script>
 
@@ -17,8 +36,15 @@ export default {
   margin:0;
   padding: 0;
 }
-#bookmarks{
+#bookmarks-PC{
   width: 80%;
+  background-color: #BDBDBD;
+  display: flex;
+  justify-content: center;
+  flex-flow: wrap;
+}
+#bookmarks-moblic{
+  width: 100%;
   background-color: #BDBDBD;
   display: flex;
   justify-content: center;
